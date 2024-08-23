4c65692c9d4adfa054a1370715944f432361fdd3
Guilherme <guilimalb@gmail.com>
2024-08-23 11:45:50 -0300

    primeiro exercicio

---
+++ src/main/java/org/example/Atv3Ex1.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Atv3Ex1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double x, soma = 0;
+        int cont;
+
+        for (cont = 0; cont < 5; cont++) {
+            System.out.print("Informe o número que você deseja somar: ");
+            x = scanner.nextDouble();
+            soma += x;
+        }
+
+        System.out.println("O somatório é: " + soma);
+        scanner.close();
+    }
+}
+
